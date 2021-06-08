28d7daf2affb2007628926d057c0bcf30955fd1a
朱伟豪 <1595724343@qq.com>
2021-06-08 20:04:12 +0800

    测试master2

--- src/main/java/com/atgui/TestGit.java
+++ src/main/java/com/atgui/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("1111111111");
         System.out.println("master");
+        System.out.println("master master");
     }
 }
