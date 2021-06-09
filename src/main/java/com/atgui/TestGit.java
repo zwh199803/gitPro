0e730d0eff7a1b88965cae72c01ccda4ce9edbbb
朱伟豪 <1595724343@qq.com>
2021-06-09 19:44:31 +0800

    测试提交代码到gitee2

--- src/main/java/com/atgui/TestGit.java
+++ src/main/java/com/atgui/TestGit.java
@@ -7,5 +7,6 @@ public class TestGit {
         System.out.println("master master");
         System.out.println("zwh");
         System.out.println("123456");
+        System.out.println("1111111");
     }
 }
